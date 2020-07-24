fa0a9b03ec276cf35866d30aacedaee309bca849
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-07-24 06:02:21 -0400

    Update minprec.jl

--- src/support/minprec.jl
+++ src/support/minprec.jl
@@ -1,5 +1,5 @@
 # protect minimum precision
-minprecerror() = throw(DomainError("minimum precision is 24 bits"))
+minprecerror() = throw(DomainError("minimum precision is $MINIMUM_PRECISION_BASE2 bits"))
 
 for T in (:Float32, :Float64, :Int16, :Int32, :Int64, :Int128, :BigFloat)
   for N in collect(0:23)
